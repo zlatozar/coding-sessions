9ddcf620429391f26f57c97256f92cf78e23d703
Zlatozar Zhelyazkov <zlatozar@gmail.com>
2014-11-28 11:13:59 +0200

    Remove explore-cs
    Signed-off-by: Zlatozar Zhelyazkov <zlatozar@gmail.com>

--- books-code/explore-cs/test/programming-the-computer-test.scm
+++
@@ -1,9 +0,0 @@
-(load-relative "../test-manager/load")
-
-(in-test-group 
-programming-the-computer
-
- (define-each-test
-   (assert-= 8 (new-double)  "Just test if test framework is set up correctly")) )
-
-(run-registered-tests)
