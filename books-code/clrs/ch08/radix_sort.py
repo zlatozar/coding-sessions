7df6e905bef41a55bfb3c0182f77177dc14146f3
Zlatozar Zhelyazkov <zlatozar@gmail.com>
2016-11-07 12:44:18 +0200

    Finish with radix sort

--- books-code/clrs/ch08/radix_sort.py
+++ books-code/clrs/ch08/radix_sort.py
@@ -7,45 +7,55 @@
 # ___________________________________________________________
 #                                                      NOTES
 
-# In radix sort numbers must have equal numbers. The interesting part is how to unify them
-# in the stable sort algorithm.
+# It is radix sort because this algorithm compares numbers for a given radix starting from
+# the least significant digit. The interesting part is how to arrange this. We need two formulas.
+
+# How to iterate through every numbers digit? Of course biggest is the leading.
+#
+# key = max(A)
+# while key / radix > 0:
+#     COUNTING_SORT(A, radix)
+#     radix = radix * 10
+#
+
+# How to select digit using given radix?
+# idx = (A[j] / radix) % 10 e.g 123 / 10 = 12, 12 % 10 = 2
 
 # ___________________________________________________________
 #                                             IMPLEMENTATION
 
-# the digit represented by exp.
-def COUNTING_SORT(A, d):
+def COUNTING_SORT(A, radix):
 
     B = [0] * len(A)
-    C = [0] * (10)
+    C = [0] * (10)    # !!!
 
-    for i in range(0, len(A)):
-        idx = (A[i] / d) % 10
+    for j in range(0, len(A)):
+        idx = (A[j] / radix) % 10
         C[idx] = C[idx] + 1
 
     for i in range(1, 10):
         C[i] = C[i - 1] + C[i]
 
     for j in range(len(A) - 1, -1, -1):
-        idx = (A[j] / d) % 10
+        idx = (A[j] / radix) % 10
         B[C[idx] - 1] = A[j]
         C[idx] = C[idx] - 1
 
-    i = 0
-    for i in range(0,len(A)):
+    # additional step
+    for i in range(0, len(A)):
         A[i] = B[i]
 
 def RADIX_SORT(A):
 
+    radix = 1
     key = max(A)
 
     # Do counting sort for every digit. Note that instead
-    # of passing digit number, exp is passed. exp is 10^i
-    # where i is current digit number
-    exp = 1
-    while key/exp > 0:
-        COUNTING_SORT(A, exp)
-        exp = exp * 10
+    # of passing digit number, 'radix' is passed. 'radix' is [1, 10, 100...10*n]
+
+    while key / radix > 0:
+        COUNTING_SORT(A, radix)
+        radix = radix * 10
 
 # ___________________________________________________________
 #                                                       TEST
